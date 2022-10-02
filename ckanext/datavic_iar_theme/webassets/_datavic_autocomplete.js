ec5582c5dc48817f165fe9533acd1947be5b2a9e
alexmorev <alelsey.morev@linkdigital.com.au>
2022-10-02 23:22:23 +0300

    SXDEDPCXZIC-59_DATAVIC-463 / add aria-required

--- ckanext/datavic_iar_theme/webassets/_datavic_autocomplete.js
+++ ckanext/datavic_iar_theme/webassets/_datavic_autocomplete.js
@@ -23,6 +23,22 @@ ckan.module("-datavic-autocomplete", function ($) {
       newRole
     );
       target.attr("role", newRole);
-      target.attr("aria-expanded", "false")
+      target.attr("aria-expanded", "false");
+      target.prop("required", true);
+      target.attr("aria-required", "true");
+
+    $('.select2-search, .select2-search-field').children('input[type="text"]').on('focusin, change', function() {
+      $(this).prop('required', true);
+      $(this).attr('aria-required', 'true');
+    });
+
+    // add aria-required attribute
+    $('#field-organizations').prop('required', true);
+    $('#field-organizations').attr('aria-required', 'true');
+    $('#field-license_id').prop('required', true);
+    $('#field-license_id').attr('aria-required', 'true');
+    $('#field-format').prop('required', true);
+    $('#field-format').attr('aria-required', 'true');
+    
   };
 });
