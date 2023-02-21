d1e972df96c4612513e46036b2f77d6e157ae125
mutantsan <kvaqich@gmail.com>
2023-02-21 10:30:40 +0200

    SXDEDPCXZIC-120_DATAVIC-472 / black project

--- ckanext/datavic_iar_theme/helpers.py
+++ ckanext/datavic_iar_theme/helpers.py
@@ -13,26 +13,28 @@ log = logging.getLogger(__name__)
 
 
 def organization_list():
-    return toolkit.get_action('organization_list')({}, {"all_fields": True, "include_dataset_count": False})
+    return toolkit.get_action("organization_list")(
+        {}, {"all_fields": True, "include_dataset_count": False}
+    )
 
 
 def get_parent_orgs(output=None):
-    organisations = model.Group.get_top_level_groups('organization')
+    organisations = model.Group.get_top_level_groups("organization")
 
-    if output == 'list':
+    if output == "list":
         parent_orgs = [org.name for org in organisations]
     else:
-        parent_orgs = [
-            {'value': '', 'text': 'Please select...'}
-        ]
+        parent_orgs = [{"value": "", "text": "Please select..."}]
         for org in organisations:
-            parent_orgs.append({'value': org.name, 'text': org.display_name})
+            parent_orgs.append({"value": org.name, "text": org.display_name})
 
     return parent_orgs
 
 
 def search_form_group_list():
-    return toolkit.get_action('group_list')({}, {'all_fields': True, "include_dataset_count": False})
+    return toolkit.get_action("group_list")(
+        {}, {"all_fields": True, "include_dataset_count": False}
+    )
 
 
 def format_list(limit=100):
@@ -40,16 +42,21 @@ def format_list(limit=100):
     try:
         session = model.Session
 
-        query = (session.query(
-            model.Resource.format,)
-            .filter(_and_(
-                model.Resource.state == 'active',
-            ))
+        query = (
+            session.query(
+                model.Resource.format,
+            )
+            .filter(
+                _and_(
+                    model.Resource.state == "active",
+                )
+            )
             .group_by(model.Resource.format)
-            .order_by(
-                func.lower(model.Resource.format)
-        ))
-        resource_formats = [resource.format for resource in query if not resource.format == '']
+            .order_by(func.lower(model.Resource.format))
+        )
+        resource_formats = [
+            resource.format for resource in query if not resource.format == ""
+        ]
     except Exception as e:
         log.error(e.message)
 
@@ -57,24 +64,24 @@ def format_list(limit=100):
 
 
 def get_parent_site_url():
-    return config.get('ckan.parent_site_url', 'https://www.data.vic.gov.au')
+    return config.get("ckan.parent_site_url", "https://www.data.vic.gov.au")
 
 
 def hotjar_tracking_enabled():
-    return toolkit.asbool(config.get('ckan.tracking.hotjar_enabled', False))
+    return toolkit.asbool(config.get("ckan.tracking.hotjar_enabled", False))
 
 
 def get_hotjar_hsid():
-    return config.get('ckan.tracking.hotjar.hjid', None)
+    return config.get("ckan.tracking.hotjar.hjid", None)
 
 
 def get_hotjar_hjsv():
-    return config.get('ckan.tracking.hotjar.hjsv', None)
+    return config.get("ckan.tracking.hotjar.hjsv", None)
 
 
 def get_gtm_code():
     # To get Google Tag Manager Code
-    gtm_code = config.get('ckan.google_tag_manager.gtm_container_id', False)
+    gtm_code = config.get("ckan.google_tag_manager.gtm_container_id", False)
     return str(gtm_code)
 
 
@@ -90,44 +97,42 @@ def datavic_linked_user(user, maxlength=0, avatar=20):
         displayname = user.display_name
 
         if maxlength and len(user.display_name) > maxlength:
-            displayname = displayname[:maxlength] + '...'
+            displayname = displayname[:maxlength] + "..."
 
-        return h.literal(u'{icon} {link}'.format(
-            icon=h.gravatar(
-                email_hash=user.email_hash,
-                size=avatar
-            ),
-            link=h.link_to(
-                displayname,
-                # DataVic custom changes to show different links depending on user access
-                h.url_for('user.read', id=name) if h.check_access('package_create') else h.url_for('user.activity', id=name)
+        return h.literal(
+            "{icon} {link}".format(
+                icon=h.gravatar(email_hash=user.email_hash, size=avatar),
+                link=h.link_to(
+                    displayname,
+                    # DataVic custom changes to show different links depending on user access
+                    h.url_for("user.read", id=name)
+                    if h.check_access("package_create")
+                    else h.url_for("user.activity", id=name),
+                ),
             )
-        ))
+        )
 
 
 def visibility_list():
     return [
         {"value": "all", "label": "Open to the public and VPS only"},
         {"value": "private", "label": "Open to VPS only"},
-        {"value": "public", "label": "Open to the public"}
+        {"value": "public", "label": "Open to the public"},
     ]
 
 
 def featured_resource_preview(package):
     # To get a featured preview for the dataset
     featured_preview = None
-    if package.get('nominated_view_id',None):
+    if package.get("nominated_view_id", None):
         try:
-            resource_view = toolkit.get_action('resource_view_show')(
-                {}, {'id': package['nominated_view_id']})
-            resource = toolkit.get_action('resource_show')(
-                {}, {'id': resource_view['resource_id']})
-            featured_preview = {
-                            'preview':resource_view,
-                            'resource':resource
-                            }
+            resource_view = toolkit.get_action("resource_view_show")(
+                {}, {"id": package["nominated_view_id"]}
+            )
+            resource = toolkit.get_action("resource_show")(
+                {}, {"id": resource_view["resource_id"]}
+            )
+            featured_preview = {"preview": resource_view, "resource": resource}
         except NotFound:
             pass
     return featured_preview
-
-
--- ckanext/datavic_iar_theme/plugin.py
+++ ckanext/datavic_iar_theme/plugin.py
@@ -10,35 +10,35 @@ class DatavicIARThemePlugin(plugins.SingletonPlugin):
     # IConfigurer
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('webassets', 'datavic_iar_theme')
+        toolkit.add_template_directory(config_, "templates")
+        toolkit.add_public_directory(config_, "public")
+        toolkit.add_resource("webassets", "datavic_iar_theme")
 
         toolkit.add_ckan_admin_tab(
             toolkit.config,
-            'check_link.report',
-            'Link availability',
-            config_var='ckan.admin_tabs',
+            "check_link.report",
+            "Link availability",
+            config_var="ckan.admin_tabs",
             icon="chain-broken",
         )
 
     # ITemplateHelpers
 
     def get_helpers(self):
-        ''' Return a dict of named helper functions (as defined in the ITemplateHelpers interface).
+        """Return a dict of named helper functions (as defined in the ITemplateHelpers interface).
         These helpers will be available under the 'h' thread-local global object.
-        '''
+        """
         return {
-            'organization_list': helpers.organization_list,
-            'get_parent_orgs': helpers.get_parent_orgs,
-            'search_form_group_list': helpers.search_form_group_list,
-            'format_list': helpers.format_list,
-            'get_parent_site_url': helpers.get_parent_site_url,
-            'get_gtm_code': helpers.get_gtm_code,
-            'datavic_linked_user': helpers.datavic_linked_user,
-            'hotjar_tracking_enabled': helpers.hotjar_tracking_enabled,
-            'get_hotjar_hsid': helpers.get_hotjar_hsid,
-            'get_hotjar_hjsv': helpers.get_hotjar_hjsv,
-            'visibility_list': helpers.visibility_list,
-            'featured_resource_preview':helpers.featured_resource_preview
+            "organization_list": helpers.organization_list,
+            "get_parent_orgs": helpers.get_parent_orgs,
+            "search_form_group_list": helpers.search_form_group_list,
+            "format_list": helpers.format_list,
+            "get_parent_site_url": helpers.get_parent_site_url,
+            "get_gtm_code": helpers.get_gtm_code,
+            "datavic_linked_user": helpers.datavic_linked_user,
+            "hotjar_tracking_enabled": helpers.hotjar_tracking_enabled,
+            "get_hotjar_hsid": helpers.get_hotjar_hsid,
+            "get_hotjar_hjsv": helpers.get_hotjar_hjsv,
+            "visibility_list": helpers.visibility_list,
+            "featured_resource_preview": helpers.featured_resource_preview,
         }
